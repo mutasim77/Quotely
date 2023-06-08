9291c4a422ded0859e25f2ea2ae45bfec9ac8d9a
Mutasim <qmutasim77@gmail.com>
2023-06-08 12:37:09 +0600

    Updated

---
+++ base/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import *
+
+urlpatterns = [
+    # Main
+    path('', main, name='main'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
